1ba7fe7a89e5d56ca81e08db3f60181ab2ee60b9
Oleg Vasilevsky <89103905+LEGO23@users.noreply.github.com>
2022-11-27 18:47:10 +0300

    Add files via upload

---
+++ RealTime/client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './app.css';
+import WebSock from './WebSock';
+// import EventSourcing from './EventSourcing';
+// import LongPulling from './LongPulling';
+
+function App() {
+  return (
+    <div>
+      <WebSock/>
+    </div>
+  )
+}
+
+export default App;
